72752d882e7ede7343dd4e8a7214eef6a14232b1
terrencebeckham <terrencebeckham@icloud.com>
2021-01-23 13:09:52 +0200

    initial commit

---
+++ call_your_mother/android/app/src/main/kotlin/com/afrodroid/call_your_mother/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.afrodroid.call_your_mother
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first_assignment/lib/text.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+var quipsList = [
+  'This is pretty cool',
+  'Stop touching me',
+  'That tickles',
+  'Enough already'
+];
